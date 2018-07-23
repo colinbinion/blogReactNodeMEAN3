e5e5dd7d94508c5743b61ce224528fa4e9d3cb5c
Colin Binion <colin.binion@me.com>
2018-07-23 17:25:01 -0600

    created mongoDB and added/tested google auth

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,6 @@
 module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientID: '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://TestUser1:Test123@ds147011.mlab.com:47011/dev_blog',
   cookieKey: '123123123'
 };
