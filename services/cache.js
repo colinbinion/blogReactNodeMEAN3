8c7d9340f49e9fadc2783a5332bcfea6c5682e01
Colin Binion <colin.binion@me.com>
2018-07-25 16:24:50 -0600

    testing key results

--- services/cache.js
+++ services/cache.js
@@ -5,12 +5,22 @@ const exec = mongoose.Query.prototype.exec;
 
 // overwrite and execute additional code when query is executed.
 mongoose.Query.prototype.exec = function () {
+  // console.log('====================================');
+  // console.log('ABOUT TO RUN A QUERY');
+  // console.log('====================================');
+  // console.log(this.getQuery());
+  // console.log('====================================');
+  // console.log(this.mongooseCollection.name);
+  // console.log('====================================');
+
+  // don't modify this.getQuery directly.  It will modify the underlying code. get object passed in to getQuery and add collection name.
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  });
+
   console.log('====================================');
-  console.log('ABOUT TO RUN A QUERY');
+  console.log(key);
   console.log('====================================');
-  console.log(this.getQuery());
-  console.log('====================================');
-  console.log(this.mongooseCollection.name);
-  console.log('====================================');
+
   return exec.apply(this, arguments);
 }
