8dd48c0e4a33fb7d1a68ca08db705c2f24db7de6
Colin Binion <colin.binion@me.com>
2018-07-25 15:49:38 -0600

    mongoose exec patching proofing

--- services/cache.js
+++ services/cache.js
@@ -1,4 +1,12 @@
 const mongoose = require('mongoose');
 
 // get ref to existing default .exec function defined on a mongoose query.
+const exec = mongoose.Query.prototype.exec;
+
 // overwrite and execute additional code when query is executed.
+mongoose.Query.prototype.exec = function () {
+  console.log('====================================');
+  console.log('ABOUT TO RUN A QUERY');
+  console.log('====================================');
+  return exec.apply(this, arguments);
+}
