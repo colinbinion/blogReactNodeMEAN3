58f2c55e244ae33a2ae4a9a8ea14edf4c4a061d1
Colin Binion <colin.binion@me.com>
2018-07-25 14:14:21 -0600

    added .logs for cache vs db test

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,60 +4,67 @@ const requireLogin = require('../middlewares/requireLogin');
 const Blog = mongoose.model('Blog');
 
 module.exports = app => {
-    app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-      const blog = await Blog.findOne({
-        _user: req.user.id,
-        _id: req.params.id
-      });
-
-      res.send(blog);
+  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+    const blog = await Blog.findOne({
+      _user: req.user.id,
+      _id: req.params.id
     });
 
-    // writing test for redis handler
-    app.get('/api/blogs', requireLogin, async (req, res) => {
-        const redis = require('redis');
-        const redisUrl = 'redis://127.0.0.1:6379';
-        const client = redis.createClient(redisUrl);
-        const util = require('util');
-        // creates promise/extends function
-        client.get = util.promisify(client.get);
+    res.send(blog);
+  });
 
-        // any cached data in redis related to this query?
-        const cachedBlogs = await client.get(req.user.id);
+  // writing test for redis handler
+  app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+    // creates promise/extends function
+    client.get = util.promisify(client.get);
 
-        // yes, responds immediately and return
-        if (cachedBlogs) {
-          // parse back to user
-          return res.send(JSON.parse(cachedBlogs);
-          }
+    // any cached data in redis related to this query?
+    const cachedBlogs = await client.get(req.user.id);
 
-          // no, respond to req and update cache to store data.  else statement not needed bc the return statement will not continue on. make sure to stringify and parse for redis
+    // yes, responds immediately and return
+    if (cachedBlogs) {
+      // parse back to user
+      console.log('====================================');
+      console.log('SERVING FROM CACHE');
+      console.log('====================================');
+      return res.send(JSON.parse(cachedBlogs));
+    }
 
-          const blogs = await Blog.find({
-            _user: req.user.id
-          });
+    // no, respond to req and update cache to store data.  else statement not needed bc the return statement will not continue on. make sure to stringify and parse for redis
 
-          res.send(blogs);
-          client.set(req.user.id, JSON.stringify(blogs));
-        });
+    const blogs = await Blog.find({
+      _user: req.user.id
+    });
 
-      app.post('/api/blogs', requireLogin, async (req, res) => {
-        const {
-          title,
-          content
-        } = req.body;
+    console.log('====================================');
+    console.log('SERVING FROM MONGODB');
+    console.log('====================================');
+    res.send(blogs);
 
-        const blog = new Blog({
-          title,
-          content,
-          _user: req.user.id
-        });
+    client.set(req.user.id, JSON.stringify(blogs));
+  });
 
-        try {
-          await blog.save();
-          res.send(blog);
-        } catch (err) {
-          res.send(400, err);
-        }
-      });
-    };
+  app.post('/api/blogs', requireLogin, async (req, res) => {
+    const {
+      title,
+      content
+    } = req.body;
+
+    const blog = new Blog({
+      title,
+      content,
+      _user: req.user.id
+    });
+
+    try {
+      await blog.save();
+      res.send(blog);
+    } catch (err) {
+      res.send(400, err);
+    }
+  });
+};
