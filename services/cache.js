e9dcd53d9d654f6dbb1deba53dea0b86a19f5b43
Colin Binion <colin.binion@me.com>
2018-07-25 16:41:48 -0600

    cache implemented

--- services/cache.js
+++ services/cache.js
@@ -11,7 +11,7 @@ client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
 // overwrite and execute additional code when query is executed.
-mongoose.Query.prototype.exec = function () {
+mongoose.Query.prototype.exec = async function () {
   // console.log('====================================');
   // console.log('ABOUT TO RUN A QUERY');
   // console.log('====================================');
@@ -21,17 +21,24 @@ mongoose.Query.prototype.exec = function () {
   // console.log('====================================');
 
   // don't modify this.getQuery directly.  It will modify the underlying code. get object passed in to getQuery and add collection name.
-  const key = Object.assign({}, this.getQuery(), {
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name
-  });
+  }));
   // console.log('====================================');
   // console.log(key);
   // console.log('====================================');
 
   // verify we have value for 'key' in redis
+  const cacheValue = await client.get(key);
   // if yes, return key
+  if (cacheValue) {
+    console.log('====================================');
+    console.log(cacheValue);
+    console.log('====================================');
+  }
   //otherwise issue query and store result in redis
-
-
-  return exec.apply(this, arguments);
+  const result = await exec.apply(this, arguments);
+  console.log('====================================');
+  console.log(result);
+  console.log('====================================');
 }
