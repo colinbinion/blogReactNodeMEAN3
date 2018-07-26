ab5284f4a41ddb859a8cbe707bff44bc9287b983
Colin Binion <colin.binion@me.com>
2018-07-26 01:28:58 -0600

    added/tested chromium instances in false

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,14 @@
+const puppeteer = require('puppeteer');
+
 test('adds two numbers', () => {
   const sum = 1 + 2;
 
   expect(sum).toEqual(3);
-})
+});
+
+test('launching browser', async () => {
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+});
