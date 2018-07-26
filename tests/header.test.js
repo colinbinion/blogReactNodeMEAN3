887a7f7db76dc9d9a1bf751dad80467a2014f5b3
Colin Binion <colin.binion@me.com>
2018-07-26 01:59:14 -0600

    fixed testing error

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,19 +1,25 @@
 const puppeteer = require('puppeteer');
 
-test('adds two numbers', () => {
-  const sum = 1 + 2;
+let browser, page;
 
-  expect(sum).toEqual(3);
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false
+  });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+
 });
 
 test('launching browser', async () => {
-  const browser = await puppeteer.launch({
-    headless: false
-  });
-  const page = await browser.newPage();
-  await page.goto('localhost:3000');
-
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  // const browser = await puppeteer.launch({
+  //   headless: false
+  // });
+  // const page = await browser.newPage();
+  // await page.goto('localhost:3000');
+
+  // const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
   expect(text).toEqual('Blogster');
 });
