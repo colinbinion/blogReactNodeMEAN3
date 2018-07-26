690d0f46c7d38f7e821723c66165a99d7dcb4fc5
Colin Binion <colin.binion@me.com>
2018-07-26 14:20:23 -0600

    added global jest setup

--- tests/header.test.js
+++ tests/header.test.js
@@ -18,14 +18,6 @@ afterEach(async () => {
 
 test('header has correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
-  // const browser = await puppeteer.launch({
-  //   headless: false
-  // });
-  // const page = await browser.newPage();
-  // await page.goto('localhost:3000');
-
-  // const text = await page.$eval('a.brand-logo', el => el.innerHTML);
-
   expect(text).toEqual('Blogster');
 });
 
